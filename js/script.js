82f9c78a985ea38244ca4396717e5ec26cb8e95d
Alessio2021 <alessiobut1911@gmail.com>
2021-11-19 14:34:54 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -11,22 +11,27 @@ console.log(priceForKilometers);
 
 // va applicato uno sconto del 20 % per i minorenni
 // va applicato uno sconto del 40 % per gli over 65.
+// L’output del prezzo finale va messo fuori in forma umana(con massimo due decimali, per indicare centesimi sul prezzo).Questo richiederà un minimo di ricerca.
 
 if (age < 18){
     discount = ((priceForKilometers / 100) * 20)
     console.log(discount);
-    price = priceForKilometers - discount;
+    price = (priceForKilometers - discount).toFixed(2);
     console.log(price);
 } else if (age > 65){
     discount = ((priceForKilometers / 100) * 40)
     console.log(discount);
-    price = priceForKilometers - discount;
+    price = (priceForKilometers - discount).toFixed(2);
     console.log(price);
 } else {
-    price = priceForKilometers;
+    price = priceForKilometers.toFixed(2);
     console.log(price);
 }
 
 
+let text = `
+<h1> Il prezzo totale del biglietto è di: ${price} </h1>
+`
 
-// L’output del prezzo finale va messo fuori in forma umana(con massimo due decimali, per indicare centesimi sul prezzo).Questo richiederà un minimo di ricerca.
+document.getElementById("my_js").innerHTML = text
+
